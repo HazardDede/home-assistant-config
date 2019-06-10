f73bf51453a092ac7ea797032829d460b80734ab
Dennis Muth <d.muth@gmx.net>
2019-06-10 15:36:21 +0200

    Improves check the fake url of waste bin sensor

--- check.sh
+++ check.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+sed -i.org "s/url: '.*'/url: 'http:\/\/dummy\.de'/" ./packages/outdoor/waste_bin.yaml
+
 hass -c . --script check_config && \
     python3 -c 'import yaml,sys;yaml.safe_load(sys.stdin)' < ui-lovelace.yaml
+
+rm packages/outdoor/waste_bin.yaml && \
+    mv packages/outdoor/waste_bin.yaml.org packages/outdoor/waste_bin.yaml

a88bc1ab038a263ae97bd6afbd84f83b8af5208b
Dennis Muth <d.muth@gmx.net>
2020-03-10 20:35:05 +0100

    Makes mqtt-bridge an optional dev component

--- run-dev.sh
+++ run-dev.sh
@@ -3,8 +3,8 @@
 mv packages/_system/guest_wifi.yaml packages/_system/guest_wifi.yaml.ignore
 mv packages/devices/pumbaa.yaml packages/devices/pumbaa.yaml.ignore
 
-docker-compose -f ./.dev/docker-compose.yaml up hass
-docker-compose -f ./.dev/docker-compose.yaml stop hass
+docker-compose -f ./.dev/docker-compose.yaml up hass $@
+docker-compose -f ./.dev/docker-compose.yaml stop hass $@
 docker-compose -f ./.dev/docker-compose.yaml down
 
 mv packages/_system/guest_wifi.yaml.ignore packages/_system/guest_wifi.yaml

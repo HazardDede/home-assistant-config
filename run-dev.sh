c1b905c18520b8e8df2daa3c6b501782704cdbb5
Dennis Muth <d.muth@gmx.net>
2020-05-05 09:49:26 +0200

    Enables guest-wifi for dev instance

--- run-dev.sh
+++ run-dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mv packages/_system/guest_wifi.yaml packages/_system/guest_wifi.yaml.ignore
+# mv packages/_system/guest_wifi.yaml packages/_system/guest_wifi.yaml.ignore
 mv packages/vacuum/pumbaa.yaml packages/vacuum/pumbaa.yaml.ignore
 
 docker-compose -f ./.dev/docker-compose.yaml up hass $@
 docker-compose -f ./.dev/docker-compose.yaml stop hass $@
 docker-compose -f ./.dev/docker-compose.yaml down
 
-mv packages/_system/guest_wifi.yaml.ignore packages/_system/guest_wifi.yaml
+# mv packages/_system/guest_wifi.yaml.ignore packages/_system/guest_wifi.yaml
 mv packages/vacuum/pumbaa.yaml.ignore packages/vacuum/pumbaa.yaml

0e3177ca551173fcac6302e36551b6c72fb994ab
Dennis Muth <d.muth@gmx.net>
2021-03-24 20:50:28 +0100

    Removes ui configured devices from yaml mode

--- run-dev.sh
+++ run-dev.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-mv packages/hubs/roomba.yaml packages/hubs/roomba.yaml.ignore
 mv packages/logic/vacuum_pumbaa.yaml packages/logic/vacuum_pumbaa.ignore
-# mv packages/hubs/homeconnect.yaml packages/hubs/homeconnect.yaml.ignore
 
 docker-compose -f ./.dev/docker-compose.yaml up hass $@
 docker-compose -f ./.dev/docker-compose.yaml stop hass $@
 docker-compose -f ./.dev/docker-compose.yaml down
 
 mv packages/logic/vacuum_pumbaa.ignore packages/logic/vacuum_pumbaa.yaml
-mv packages/hubs/roomba.yaml.ignore packages/hubs/roomba.yaml
-# mv packages/hubs/homeconnect.yaml.ignore packages/hubs/homeconnect.yaml

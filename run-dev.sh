c455feeb38a340bf84dfa894fb8078209d8dd327
Dennis Muth <d.muth@gmx.net>
2022-12-05 11:12:57 +0100

    Removes pumbaa from config

--- run-dev.sh
+++ run-dev.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-mv packages/logic/vacuum_pumbaa.yaml packages/logic/vacuum_pumbaa.ignore
-
 docker-compose -f ./.dev/docker-compose.yaml up hass $@
 docker-compose -f ./.dev/docker-compose.yaml stop hass $@
 docker-compose -f ./.dev/docker-compose.yaml down
-
-mv packages/logic/vacuum_pumbaa.ignore packages/logic/vacuum_pumbaa.yaml
